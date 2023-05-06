977963874a43c33e083d5bf40e45dd9f5d121b86
mounika <reddymounika2805@gmail.com>
2023-05-06 20:05:26 +0530

    login2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
-function App() {
+function LoginForm() {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const [isLoginError, setIsLoginError] = useState(false);
+
+  const onSubmit = (data) => {
+   
+    console.log(data);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>
+        Email
+        <input type="email" {...register("email", { required: true })} />
+        {errors.email && <span>This field is required</span>}
+      </label>
+      <label>
+        Password
+        <input type="password" {...register("password", { required: true, minLength: 5, pattern: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{5,}$/ })} />
+        {errors.password?.type === "required" && <span>This field is required</span>}
+        {errors.password?.type === "minLength" && <span>Password must be at least 5 characters long</span>}
+        {errors.password?.type === "pattern" && <span>Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character</span>}
+      </label>
+      {isLoginError && <span>Login failed. Please try again.</span>}
+      <button type="submit">Login</button>
+    </form>
   );
 }
 
-export default App;
+export default LoginForm;
